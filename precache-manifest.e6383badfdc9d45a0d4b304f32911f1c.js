7dba7a18175e86419cb1bc1e6d88f21dd16d7b1b
Nikita Korobkov <nikkorobk@gmail.com>
2019-12-17 17:20:47 -0500

    Updates

---
+++ precache-manifest.e6383badfdc9d45a0d4b304f32911f1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44e31d0cf099d60bcd8fa6d9d9be07d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4837eea044b723212ce7",
+    "url": "/static/css/main.ba6b73de.chunk.css"
+  },
+  {
+    "revision": "8d26ddaef19cc41513fe",
+    "url": "/static/js/2.91fe2167.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/static/js/2.91fe2167.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4837eea044b723212ce7",
+    "url": "/static/js/main.281c36c6.chunk.js"
+  },
+  {
+    "revision": "931cec1457a9d6297ee2",
+    "url": "/static/js/runtime-main.a4b9b745.js"
+  }
+]);
